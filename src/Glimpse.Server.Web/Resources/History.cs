d53585b3813127166049fac8633fcfbf8f919b10
Nik Molnar <nikmd23@gmail.com>
2015-09-10 11:01:29 -0400

    Split History into two resources

--- src/Glimpse.Server.Web/Resources/History.cs
+++ src/Glimpse.Server.Web/Resources/History.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Glimpse.Server.Web.Extensions;
 using Microsoft.AspNet.Http;
@@ -8,98 +6,28 @@ using Microsoft.Net.Http.Headers;
 
 namespace Glimpse.Server.Web
 {
-    public class History : IResource
+    public class MessageHistoryResource : IResource
     {
         private readonly IStorage _store;
-        private readonly IQueryRequests _requests;
-        private readonly bool _canQueryRequests;
 
-        public History(IStorage storage)
+        public MessageHistoryResource(IStorage storage)
         {
             _store = storage;
-            _requests = storage.As<IQueryRequests>();
-
-            _canQueryRequests = storage.Supports<IQueryRequests>();
-            if (_canQueryRequests)
-                Parameters = new ResourceParameters(
-                    ResourceParameter.Custom("dmin"),
-                    ResourceParameter.Custom("dmax"),
-                    ResourceParameter.Custom("url"),
-                    ResourceParameter.Custom("methods"),
-                    ResourceParameter.Custom("smin"),
-                    ResourceParameter.Custom("smax"),
-                    ResourceParameter.Custom("tags"),
-                    ResourceParameter.Custom("before"),
-                    ResourceParameter.Custom("user"),
-                    ResourceParameter.Custom("types"));
-            else
-                Parameters = new ResourceParameters(ResourceParameter.Custom("types"));
-            
         }
 
-        // TODO: The parameter parsing in this method is brittle and will require more work
         public async Task Invoke(HttpContext context, IDictionary<string, string> parameters)
         {
             var response = context.Response;
             response.Headers[HeaderNames.ContentType] = "application/json";
 
-            IEnumerable<string> list;
-            if (!_canQueryRequests)
-            {
-                list = await _store.RetrieveByType(parameters["types"].Split(','));
-            }
-            else
-            {
-                var filters = new RequestFilters();
-                if (parameters.ContainsKey("dmin"))
-                    filters.DurationMinimum = float.Parse(parameters["dmin"]);
+            var list = await _store.RetrieveByType(parameters["types"].Split(','));
 
-                if (parameters.ContainsKey("dmax"))
-                    filters.DurationMaximum = float.Parse(parameters["dmax"]);
-
-                if (parameters.ContainsKey("url"))
-                    filters.UrlContains = parameters["url"];
-
-                if (parameters.ContainsKey("methods"))
-                    filters.MethodList = parameters["methods"].Split(',');
-
-                if (parameters.ContainsKey("smin"))
-                    filters.StatusCodeMinimum = int.Parse(parameters["smin"]);
-
-                if (parameters.ContainsKey("smax"))
-                    filters.StatusCodeMaximum = int.Parse(parameters["smax"]);
-
-                if (parameters.ContainsKey("tags"))
-                    filters.TagList = parameters["tags"].Split(',');
-
-                if (parameters.ContainsKey("user"))
-                    filters.UserId = parameters["user"];
-
-                if (parameters.ContainsKey("before"))
-                    filters.RequesTimeBefore = DateTime.Parse(parameters["before"]);
-
-                if (parameters.ContainsKey("types"))
-                {
-                    var types = parameters["types"].Split(',');
-                    list = await _requests.Query(filters, types);
-                }
-                else
-                {
-                    list = await _requests.Query(filters);
-                }
-            }
-
-            var sb = new StringBuilder("[");
-            sb.Append(string.Join(",", list));
-            sb.Append("]");
-            var output = sb.ToString();
-
-            await response.WriteAsync(output);
+            await response.WriteAsync(list.ToJsonArray());
         }
 
-        public string Name => "MessageHistory";
+        public string Name => "History";
         
-        public ResourceParameters Parameters { get; }
+        public ResourceParameters Parameters => new ResourceParameters(ResourceParameter.Custom("types"));
 
         public ResourceType Type => ResourceType.Client;
     }
---
+++ src/Glimpse.Server.Web/Resources/RequestHistory.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Glimpse.Server.Web.Extensions;
+using Microsoft.AspNet.Http;
+using Microsoft.Net.Http.Headers;
+
+namespace Glimpse.Server.Web.Resources
+{
+    public class RequestHistory : IResource
+    {
+        private readonly IQueryRequests _requests;
+
+        public RequestHistory(IStorage storage)
+        {
+            if (!storage.Supports<IQueryRequests>())
+                throw new ArgumentException($"IStorage of type '{storage.GetType().AssemblyQualifiedName}' does not support IQueryRequests.", nameof(storage));
+
+            _requests = storage.As<IQueryRequests>();
+        }
+
+        // TODO: The parameter parsing in this method is brittle and will require more work
+        public async Task Invoke(HttpContext context, IDictionary<string, string> parameters)
+        {
+            var response = context.Response;
+            response.Headers[HeaderNames.ContentType] = "application/json";
+            
+            var filters = new RequestFilters();
+            if (parameters.ContainsKey("dmin"))
+                filters.DurationMinimum = float.Parse(parameters["dmin"]);
+
+            if (parameters.ContainsKey("dmax"))
+                filters.DurationMaximum = float.Parse(parameters["dmax"]);
+
+            if (parameters.ContainsKey("url"))
+                filters.UrlContains = parameters["url"];
+
+            if (parameters.ContainsKey("methods"))
+                filters.MethodList = parameters["methods"].Split(',');
+
+            if (parameters.ContainsKey("smin"))
+                filters.StatusCodeMinimum = int.Parse(parameters["smin"]);
+
+            if (parameters.ContainsKey("smax"))
+                filters.StatusCodeMaximum = int.Parse(parameters["smax"]);
+
+            if (parameters.ContainsKey("tags"))
+                filters.TagList = parameters["tags"].Split(',');
+
+            if (parameters.ContainsKey("user"))
+                filters.UserId = parameters["user"];
+
+            if (parameters.ContainsKey("before"))
+                filters.RequesTimeBefore = DateTime.Parse(parameters["before"]);
+
+            IEnumerable<string> list;
+            if (parameters.ContainsKey("types"))
+            {
+                var types = parameters["types"].Split(',');
+                list = await _requests.Query(filters, types);
+            }
+            else
+            {
+                list = await _requests.Query(filters);
+            }
+
+            await response.WriteAsync(list.ToJsonArray());
+        }
+
+        public string Name => "RequestHistory";
+
+        public ResourceParameters Parameters => new ResourceParameters(
+                    ResourceParameter.Custom("dmin"),
+                    ResourceParameter.Custom("dmax"),
+                    ResourceParameter.Custom("url"),
+                    ResourceParameter.Custom("methods"),
+                    ResourceParameter.Custom("smin"),
+                    ResourceParameter.Custom("smax"),
+                    ResourceParameter.Custom("tags"),
+                    ResourceParameter.Custom("before"),
+                    ResourceParameter.Custom("user"),
+                    ResourceParameter.Custom("types"));
+
+        public ResourceType Type => ResourceType.Client;
+    }
+}
